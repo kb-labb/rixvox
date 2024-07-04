bdba57a631ae8d95c27f484c7aacfb74e03dc04d
Lauler <fatonr@gmail.com>
2024-07-04 13:21:43 +0200

    Remove convenience script

--- mp3_to_wav.sh
+++
@@ -1,5 +0,0 @@
-# Covert an mp3 file to a wav file 16khz mono
-# Usage: ./mp3_to_wav.sh <input_mp3_file> <output_wav_file>
-# Dependencies: ffmpeg
-
-ffmpeg -i $1 -acodec pcm_s16le -ac 1 -ar 16000 $2
